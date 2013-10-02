b5ff76ed703a9299104be2fb4c0433d86cb23bd2
root <root@localhost.localdomain>
2013-10-02 02:21:31 -0400

    fix

--- install_init.sh
+++ install_init.sh
@@ -3,7 +3,7 @@ Current=${PWD};
 
 #pip_version = $(/usr/bin/pip -V | awk -F[[:space:]] '{print $2}');
 
-yum -q install python-pip python-dev python-pycurl python-simplejson nginx;
+yum install python-pip python-dev python-pycurl python-simplejson nginx;
 python ${Current}/install_supervisor.py;
 pip -q install tornado;
 
@@ -11,17 +11,18 @@ adduser -M www-data;
 #adduser -M supervisord;
 #usermod -a -G supervisord root;
 
-mkdir -p /var/log/supervisord;
+mkdir -p /var/log/supervisor;
 touch /var/run/supervisord.pid;
 #chown supervisord:supervisord /var/run/supervisord.pid 
 
 #chown supervisord:supervisord /var/log/supervisord;
-cp -r ${Current}/pythonweb /var/pythonweb;
+cp -r ${Current}/var/pythonweb /var/pythonweb;
 cp -f ${Current}/etc/supervisord.conf /etc/supervisord.conf;
 cp -f ${Current}/etc/nginx/nginx.conf /etc/nginx/nginx.conf;
-chown www-data:www-data /var/pythonweb; 
+chown -R www-data:www-data /var/pythonweb; 
 cp -f ${Current}/supervisord.service /usr/lib/systemd/system/supervisord.service;
 cp -f ${Current}/nginxd.service /usr/lib/systemd/system/nginxd.service;
 
 service nginxd enable;
 service supervisord enable;
+setsebool -P httpd_can_network_connect 1
--- install_supervisor.py
+++ install_supervisor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
+import os
 import sys
 import re
 f=open("/usr/bin/pip",'r')
 f.next()
 line=f.next()
-m=re.search('pip==([\d\.]+)',line)
-v=m.group(1)
+m=re.search('pip==(\d\.\d)',line)
+v=float(m.group(1))
 #With pip >= 1.4
 #pip -q install supervisor --pre;
 
@@ -13,5 +14,5 @@ v=m.group(1)
 #pip -q install supervisor;
 if v>=1.4:
 	os.system("pip -q install supervisor --pre")
-	else
+else:
 	os.system("pip -q install supervisor")
